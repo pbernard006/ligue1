9bf6a994e33d166e000a6ef7dcd576a9f387a660
pbernard006 <pbernard006@iutbayonne.univ-pau.fr>
2022-03-03 17:42:34 +0100

    Ajout fonctionnalités de voir les matchs précédents

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,5 +1,39 @@
-function Score() {
-    return (
+function Score({ data }) {
+  console.log({ data });
+  return (
+    <div className="container-fluid bg-match">
+      <div className="row">
+        <div className="col-6 mt-4  mb-4 text-center">
+          <span className="text-light fs-2">{data?.teams.home.name}</span>
+          <br />
+          <div className="score">
+            {data?.fixture.status.short === "NS" && (
+              <span className="text-light fs-2">0</span>
+            )}
+            {data?.fixture.status.short !== "NS" && (
+              <span className="text-light fs-2">
+                {data?.score.fulltime.home}
+              </span>
+            )}
+          </div>
+        </div>
+        <div className="col-6 mt-4 mb-4 text-center">
+          <span className="text-light fs-2 ">{data?.teams.away.name}</span>
+          <br />
+          <div className="score">
+            {data?.fixture.status.short === "NS" && (
+              <span className="text-light fs-2">0</span>
+            )}
+            {data?.fixture.status.short !== "NS" && (
+              <span className="text-light fs-2">
+                {data?.score.fulltime.away}
+              </span>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+    /* 
         <div className="container-fluid bg-match">
             <div className="row">
                 <div className="col-6 mt-4  mb-4 text-center">
@@ -18,7 +52,8 @@ function Score() {
                 </div>
             </div>
         </div>
-    );
+        */
+  );
 }
 
 export default Score;
